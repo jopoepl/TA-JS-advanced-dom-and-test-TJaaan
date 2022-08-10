e2b360ab9d26e71d62bca13b2bcf563a0589cbe9
jopoepl <joel_joseph93@outlook.com>
2022-08-10 09:04:29 +0530

    notice board assgn-update

--- block-BJaair/code/index.js
+++ block-BJaair/code/index.js
@@ -1,4 +1,5 @@
-let data = {study: "JS", play: "volleyball"};
+let data = [];
+let cardsData = JSON.parse(localStorage.getItem(`cards`)) || [];
 
 let cardsCont = document.querySelector(`.cards-cont`)
 let form = document.querySelector(`form`)
@@ -6,34 +7,68 @@ let inputTitle = document.querySelector(`#InputTitle`)
 let inputCat = document.querySelector(`#InputCat`)
 
 
-function createCard(){
-    for(let i = 0; i< Object.keys(data).length; i++){
-    let card = document.createElement(`div`);
-    card.classList.add(`card`)
-    let title = document.createElement(`h3`);
-    title.innerText = data[Object.keys(data)[i]]
-    let category = document.createElement(`h5`)
-    category.innerText = Object.keys(data)[i]
-    card.append(category, title);
-    cardsCont.append(card)
-    }
+function createCard(data, root){
+    root.innerHTML = "";
+
+    data.map((obj, index) => {
+        let card = document.createElement(`div`);
+        card.classList.add(`card`)
+        let title = document.createElement(`h3`);
+        let category = document.createElement(`h5`)
+        category.addEventListener("dblclick", () => {
+            handleEdit(event, obj.category, index, "category")
+        })
+        title.addEventListener("dblclick", () => {
+            handleEdit(event, obj.title, index, "title")
+        })
+        title.innerText = obj.title
+        category.innerText = obj.category
+        card.append(category, title);
+        root.append(card)
+    })
 }
 
 let index = 0;
 
+function handleEdit(event, info, id, label){
+    let input = document.createElement(`input`);
+    input.value = info;
+    let ele = event.target;
+    let parent = event.target.parentElement;
+    parent.replaceChild(input, ele);
+    input.addEventListener("keyup", (event) => {
+        if(event.keyCode === 13){
+        let newValue = event.target.value;
+        cardsData[id][label] = newValue;
+        createCard(cardsData, cardsCont);
+        }
+    })
+    input.addEventListener("blur", (event) => {
+        let newValue = event.target.value;
+        cardsData[id][label] = newValue;
+        createCard(cardsData, cardsCont);
+    })
+}
+
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
-    data[inputCat.value] = inputTitle.value;
-    inputCat.innerHTML =""
-    inputTitle.innerHTML =""
-    cardsCont.innerHTML =""
-    createCard();
+    let title = event.target.elements.InputTitle.value;
+    let category = event.target.elements.InputCat.value;
+    console.log(title, category)
+    cardsData.push({title, category})
+    // inputCat.innerHTML =""
+    // inputTitle.innerHTML =""
+    // cardsCont.innerHTML =""
+    localStorage.setItem(`cards`, JSON.stringify(cardsData))
+    createCard(cardsData, cardsCont);
     
 })
 
-console.log(data)
+console.log(cardsData)
+createCard(cardsData, cardsCont);
 
-cardsCont.addEventListener()
+// cardsCont.addEventListener("dblclick", handleEdit(event, update, id))
+
 
 
 
